298b31b6fa153cf7b0d97438daa2956096cd79ee
UnSamuel <60000867swim@gmail.com>
2023-11-02 12:01:59 -0400

    subiendo estructura inicial

---
+++ FinalWebApp/FinalWebApp/Context/AplicacionContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using The_Last_Dance_First_Try.Models;
+
+namespace The_Last_Dance_First_Try.Context
+{
+    public class AplicacionContext : DbContext
+    {
+        public AplicacionContext (DbContextOptions<AplicacionContext> options)
+            : base(options)
+        { 
+        
+        }
+
+        public DbSet <Departamento> Departamento { get; set; }
+        public DbSet <Empleado> Empleado { get; set; }
+        public DbSet <Salario> Salario { get; set; }
+        public DbSet <Seguro> Seguro { get; set; }
+        public DbSet <Titulo> Titulo { get; set; }
+
+
+    }
+}
